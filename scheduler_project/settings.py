cc23d067d81fe6a4cefe32be423caa583f0f9c51
manaswiniboja <Manaswini.B@proventech.in>
2026-02-13 15:20:58 +0530

    settings.py

--- scheduler_project/settings.py
+++ scheduler_project/settings.py
@@ -117,6 +117,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # your app-specific static files
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
